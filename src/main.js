25c9edd08c6cf95200eca9f6a9670df27123db96
binh tran <binh.tran@8bitrockr.com>
2019-03-06 11:02:08 +0700

    update URL_PATH

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Meta from 'vue-meta'
-import firebase from 'firebase/app'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -10,13 +9,8 @@ Vue.config.productionTip = false
 
 Vue.use(Meta)
 
-let app = null
-firebase.auth().onAuthStateChanged(() => {
-  if (!app) {
-    app = new Vue({
-      router,
-      store,
-      render: h => h(App)
-    }).$mount('#app')
-  }
-})
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
